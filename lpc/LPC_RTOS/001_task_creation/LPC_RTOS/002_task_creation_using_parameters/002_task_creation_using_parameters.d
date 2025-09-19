201a76b97f28060286c4b1f40bc0dee719a9a645
sivayekolla <sivayekollaonmail@gmail.com>
2025-09-19 13:43:12 +0530

    Add files via upload

---
+++ lpc/LPC_RTOS/001_task_creation/LPC_RTOS/002_task_creation_using_parameters/002_task_creation_using_parameters.d
@@ -0,0 +1,13 @@
+.\002_task_creation_using_parameters.o: New folder\002_task_creation_using_parameters.c
+.\002_task_creation_using_parameters.o: C:\Keil\ARM\Inc\Philips\LPC214x.h
+.\002_task_creation_using_parameters.o: C:\Keil\FreeRTOS_V9.0.0rc2\FreeRTOS\Source\include\FreeRTOS.h
+.\002_task_creation_using_parameters.o: C:\Keil\ARM\ARMCC\bin\..\include\stddef.h
+.\002_task_creation_using_parameters.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\002_task_creation_using_parameters.o: C:\Keil\FreeRTOS_V9.0.0rc2\FreeRTOS\Source\include\FreeRTOSConfig.h
+.\002_task_creation_using_parameters.o: C:\Keil\FreeRTOS_V9.0.0rc2\FreeRTOS\Source\include\projdefs.h
+.\002_task_creation_using_parameters.o: C:\Keil\FreeRTOS_V9.0.0rc2\FreeRTOS\Source\include\portable.h
+.\002_task_creation_using_parameters.o: C:\Keil\FreeRTOS_V9.0.0rc2\FreeRTOS\Source\include\deprecated_definitions.h
+.\002_task_creation_using_parameters.o: C:\Keil\FreeRTOS_V9.0.0rc2\FreeRTOS\Source\include\..\..\Source\portable\RVDS\ARM7_LPC21xx\portmacro.h
+.\002_task_creation_using_parameters.o: C:\Keil\FreeRTOS_V9.0.0rc2\FreeRTOS\Source\include\mpu_wrappers.h
+.\002_task_creation_using_parameters.o: C:\Keil\FreeRTOS_V9.0.0rc2\FreeRTOS\Source\include\task.h
+.\002_task_creation_using_parameters.o: C:\Keil\FreeRTOS_V9.0.0rc2\FreeRTOS\Source\include\list.h
